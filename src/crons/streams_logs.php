677896dfa20f6d7f6e7bbd6bb5f9c35abb2cfdd3
xneoserv <myneoserv@gmail.com>
2026-02-17 21:48:44 +0100

    Update streams_logs.php

--- src/crons/streams_logs.php
+++ src/crons/streams_logs.php
@@ -1,10 +1,10 @@
 <?php
-if (posix_getpwuid(posix_geteuid())['name'] == 'xc_vm') {
+if (posix_getpwuid(posix_geteuid())['name'] == 'neoserv') {
     set_time_limit(0);
     if ($argc) {
         register_shutdown_function('shutdown');
         require str_replace('\\', '/', dirname($argv[0])) . '/../www/init.php';
-        cli_set_process_title('XC_VM[Stream Logs]');
+        cli_set_process_title('NeoServ[Stream Logs]');
         $rIdentifier = CRONS_TMP_PATH . md5(CoreUtilities::generateUniqueCode() . __FILE__);
         CoreUtilities::checkCron($rIdentifier);
         $rLog = LOGS_TMP_PATH . 'stream_log.log';
@@ -21,7 +21,7 @@ if (posix_getpwuid(posix_geteuid())['name'] == 'xc_vm') {
         exit(0);
     }
 } else {
-    exit('Please run as XC_VM!' . "\n");
+    exit('Please run as NeoServ!' . "\n");
 }
 function parseLog($rLog) {
     $rQuery = '';
