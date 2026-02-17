a56cc258682f1d080748da3612e7c2a47c292b72
xneoserv <myneoserv@gmail.com>
2026-02-17 21:03:38 +0100

    Update daemons.sh

--- src/bin/daemons.sh
+++ src/bin/daemons.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-start-stop-daemon --start --quiet --pidfile /home/xc_vm/bin/php/sockets/1.pid --exec /home/xc_vm/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xc_vm/bin/php/etc/1.conf
-start-stop-daemon --start --quiet --pidfile /home/xc_vm/bin/php/sockets/2.pid --exec /home/xc_vm/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xc_vm/bin/php/etc/2.conf
-start-stop-daemon --start --quiet --pidfile /home/xc_vm/bin/php/sockets/3.pid --exec /home/xc_vm/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xc_vm/bin/php/etc/3.conf
-start-stop-daemon --start --quiet --pidfile /home/xc_vm/bin/php/sockets/4.pid --exec /home/xc_vm/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/xc_vm/bin/php/etc/4.conf
+start-stop-daemon --start --quiet --pidfile /home/neoserv/bin/php/sockets/1.pid --exec /home/neoserv/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/neoserv/bin/php/etc/1.conf
+start-stop-daemon --start --quiet --pidfile /home/neoserv/bin/php/sockets/2.pid --exec /home/neoserv/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/neoserv/bin/php/etc/2.conf
+start-stop-daemon --start --quiet --pidfile /home/neoserv/bin/php/sockets/3.pid --exec /home/neoserv/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/neoserv/bin/php/etc/3.conf
+start-stop-daemon --start --quiet --pidfile /home/neoserv/bin/php/sockets/4.pid --exec /home/neoserv/bin/php/sbin/php-fpm -- --daemonize --fpm-config /home/neoserv/bin/php/etc/4.conf
